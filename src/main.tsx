30d13567bfd5454211774e25c1df408af892b087
Eric <factoreric123@gmail.com>
2026-03-02 13:16:05 -0500

    Add change marker comment in main entry

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,5 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Change marker: added to verify GitHub push flow.
 createRoot(document.getElementById("root")!).render(<App />);
